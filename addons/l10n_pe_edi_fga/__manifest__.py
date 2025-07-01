5877da95409046e9aee9cd937c7f9c83692f2aff
FranjoGrande <65228110+FranjoGrande@users.noreply.github.com>
2025-06-30 23:48:42 -0500

    [ADD] l10n_pe_edi_fga: initial skeleton

---
+++ addons/l10n_pe_edi_fga/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Peru - Electronic Invoicing FGA',
+    'version': '0.1',
+    'category': 'Accounting/Localizations/EDI',
+    'summary': 'Electronic invoicing for Peru using FGA service according to SUNAT.',
+    'depends': [
+        'account_edi',
+        'l10n_pe',
+    ],
+    'data': [
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+}
